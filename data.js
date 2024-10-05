40d379462dac8a78af546a85049b3b083a411013
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-10-05 22:55:00 +0530

    Using 2 separate files for data and manipulating the DOM instead of manual typing

---
+++ data.js
@@ -0,0 +1,56 @@
+export const projectDetails = [
+    {
+        name: 'Super Cool Project',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur iure ad sint accusamus, assumenda nisi Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, inventore? perferendis corporis pariatur iste.',
+    },
+    {
+        name: 'Less Cool Project',
+        description:'Lorem ipsum dolor sit amet lorem50 consectetur adipisicing elit. Ratione tenetur iure Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aspernatur eum quo ab dolore ipsam magnam architecto fugiat? Voluptatum similique modi fuga cumque. Doloribus error reiciendis vel. Quasi, exercitationem repellendus. ad sint accusamus, assumenda nisi perferendis corporis pariatur iste.',
+    },
+    {
+        name: 'Impossible App',
+        description:'Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quibusdam, quod officiis reiciendis fugit fuga iusto maiores repudiandae, saepe quisquam, nulla qui itaque? Repellat, incidunt? Eaque soluta in unde molestiae minima amet, hic reprehenderit tempore. Commodi quibusdam a ex sequi distinctio atque. Magni soluta fugiat nostrum veritatis, illum ratione velit! adipisicing elit. Ratione tenetur iure ad sint accusamus, assumenda nisi perferendis corporis pariatur iste.',
+    },
+    {
+        name: 'Easy Peasy APP',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur iure ad sint accusamus, assumenda nisi perferendis corporis pariatur iste.',
+    },
+    {
+        name: 'AD Blocker',
+        description:'Lorem ipsum dolor Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ex minima unde? Alias voluptatum explicabo sapiente officiis voluptas ratione praesentium hic cumque nulla, laborum, harum distinctio quos reiciendis recusandae eius! sit amet consectetur adipisicing elit. Ratione tenetur iure ad sint accusamus, assumenda nisi perferendis corporis pariatur iste.',
+    },
+    {
+        name: 'Money Maker',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur iure ad sint accusamus, assumenda nisi perferendis corporis pariatur iste.',
+    },
+]
+
+export const announcment = [
+    {
+        name: 'Site Maintenance',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, earum.',
+    },
+    {
+        name: 'Community Share Day',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, earum.',
+    },
+    {
+        name: 'Updated Privacy Policy',
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, earum.',
+    },
+]
+
+export const users = [
+    {
+        name:'@tegan',
+        description:'World Peace Maker',
+    },
+    {
+        name:'@morgan',
+        description:'Cool Projects',
+    },
+    {
+        name:'@kendall',
+        description:'Life Changing Apps',
+    },
+]
